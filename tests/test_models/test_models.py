4c224f617372c1dc9634360df90fe19fd3dd597b
hileamlakb <hilea.buissness@gmail.com>
2021-02-11 13:16:26 +0300

    Add a date time obect comparision function and assert the equalness of two date times

--- tests/test_models/test_models.py
+++ tests/test_models/test_models.py
@@ -36,12 +36,17 @@ class TestBaseCorrectness(unittest.TestCase):
                 - created_at and updated_at must be converted to string object in ISO format:
                     format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
     """
-
-    def assertDateTimeEqualEqual(self, time1, time2):
+    def assertDateTimeAlmostEqual(self, time1, time2):
         """
-            checks the equalness of two date tiems
+            checks the equalness of two datetime objects
+            by comaring their year, month, date, hour, and minute but
+            not anything less than that like sec and millissecond
         """
-        pass
+        self.assertEqual(time1.year, time2.year)
+        self.assertEqual(time1.month, time2.month)
+        self.assertEqual(time1.day, time2.day)
+        self.assertEqual(time1.hour, time2.hour)
+        self.assertEqual(time1.minute, time2.minute)
 
     def setUp(self):
         """
@@ -82,18 +87,20 @@ class TestBaseCorrectness(unittest.TestCase):
         self.assertIsInstance(self.cls1.created_at, datetime)
 
         # Test if the class created_at is around the correct time
-        self.assertDateTimeEqualEqual(self.cls1_creation, self.cls1.created_at)
+        self.assertDateTimeAlmostEqual(self.cls1_creation,
+                                       self.cls1.created_at)
 
         # Tests if the updated time is also set coorrectl set
         self.assertIsInstance(self.cls1.updated_at, datetime)
-        self.assertDateTimeEqualEqual(self.cls1.created_at, self.cls1.updated_at)
+        self.assertDateTimeAlmostEqual(self.cls1.created_at,
+                                       self.cls1.updated_at)
 
         self.cls1_update = datetime.now()
         # Test the change of updtaed time with the change of attribute
         self.cls1.id = "Random string has been set to be the id"
 
         # Test if the class updated_at variable is updated corrctly with class updates
-        self.assertDateTimeEqualEqual(self.cls1_update, self.cls1.updated_at)
+        self.assertDateTimeAlmostEqual(self.cls1_update, self.cls1.updated_at)
 
     def test_str(self):
         """
@@ -116,7 +123,7 @@ class TestBaseCorrectness(unittest.TestCase):
         self.cls2.save()
 
         # Test if the class updated_at variable is updated corrctly with object updates
-        self.assertDateTimeEqualEqual(self.cls2_update, self.cls2.updated_at)
+        self.assertDateTimeAlmostEqual(self.cls2_update, self.cls2.updated_at)
 
     def test_to_obj(self):
         """
@@ -128,12 +135,14 @@ class TestBaseCorrectness(unittest.TestCase):
         # Test if "updated_at" & "created_at" are attributes of the returned dictionary
         if "updated_at" not in dict.keys():
             self.fail("updated_at Key not found in returned dictionary")
-        elif "created_at" not in dict.keys():
+        if "created_at" not in dict.keys():
             self.fail("created_at Key not found in returned dictionary")
         else:
             # Test if dict["updated_at"] & dict["created_at"] use the isotime format
-            self.assertEquals(dict["updated_at"], dict["updated_at"].isoformat())
-            self.assertEquals(dict["created_at"], dict["updated_at"].isoformat())
+            self.assertEquals(dict["updated_at"],
+                              dict["updated_at"].isoformat())
+            self.assertEquals(dict["created_at"],
+                              dict["updated_at"].isoformat())
 
         # Include extra variables to make sure they are handeld by the to_obj function
         self.cls3.name = "Random Name"
@@ -150,6 +159,7 @@ class TestBaseCorrectness(unittest.TestCase):
 
         # Test if returned dictionary from to_obj  and the excpected one are equal
         self.assertDictEqual(dict, self.cls3.to_obj())
+
 
 if __name__ == "__main__":
     unittest.main()
